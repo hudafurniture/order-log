1e1aec35e91b5e82187ff58990a260d3d9a57a53
hudafurniture <169073981+hudafurniture@users.noreply.github.com>
2024-07-14 14:58:05 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { ThemeProvider } from "../src/@/components/theme-provider";
-import { Navbar } from "./components/nav/Nav";
+import { Navbar } from "./components/Nav/Nav";
 
 function App() {
   const location = useLocation();
